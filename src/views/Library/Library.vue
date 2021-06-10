386bc1e9981c54c41ac2c2f31bf468e6b2aed88e
UBC Digital Developers <devs@ubc.digital>
2021-06-10 09:57:57 +0200

    (Bridge/Syntax) Updated @yourdlt/wallet-api-bridge and fixed Library

--- src/views/Library/Library.vue
+++ src/views/Library/Library.vue
@@ -1,35 +1,39 @@
-/** * This file is part of YourDLT Wallet Plugins shared under LGPL-3.0 *
-Copyright (C) 2021 Using Blockchain Ltd, Reg No.: 12658136, United Kingdom * *
-@package YourDLT Wallet Plugins * @subpackage Librarian * @author Grégory Saive
-for Using Blockchain Ltd
-<greg@ubc.digital>
-  * @license LGPL-3.0 */
-  <template>
-    <span>library placeholder</span>
-  </template>
+<!--
+/**
+ * This file is part of YourDLT Wallet Plugins shared under LGPL-3.0
+ * Copyright (C) 2021 Using Blockchain Ltd, Reg No.: 12658136, United Kingdom
+ *
+ * @package     YourDLT Wallet Plugins
+ * @subpackage  Librarian
+ * @author      Grégory Saive for Using Blockchain Ltd <greg@ubc.digital>
+ * @license     LGPL-3.0
+ */
+-->
+<template>
+  <span>library placeholder</span>
+</template>
 
-  <script>
-    export default {
-      data() {
-        return {
-          greeting: "Hello, Librarian",
-        };
-      },
-
-      mounted() {
-        console.log("Librarian mounted");
-      },
-
-      created() {
-        console.log("Librarian created");
-      },
+<script>
+export default {
+  data() {
+    return {
+      greeting: "Hello, Librarian",
     };
-  </script>
+  },
 
-  <style lang="css" scoped>
-    span {
-      font-weight: 800;
-      text-decoration: underline;
-    }
-  </style>
-</greg@ubc.digital>
+  mounted() {
+    console.log("Librarian mounted");
+  },
+
+  created() {
+    console.log("Librarian created");
+  },
+};
+</script>
+
+<style lang="css" scoped>
+span {
+  font-weight: 800;
+  text-decoration: underline;
+}
+</style>
